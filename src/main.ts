78ddc69b2dce3ae6020e20ab4f8395b28840c448
Santosh Yadav <santosh.yadav198613@gmail.com>
2023-05-17 11:41:58 +0000

    prod dependencies

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,14 @@ import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    const url: string = core.getInput('feedurl')
+    core.debug(`fetching data from ${url}  ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
     core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const res = await fetch(url).then((res)=> res.json());
+    core.debug(`${res}`)
 
-    core.setOutput('time', new Date().toTimeString())
+    // core.setOutput('time', new Date().toTimeString())
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
